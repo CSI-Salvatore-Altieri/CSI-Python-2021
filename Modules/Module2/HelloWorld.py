d068ba40546a0c89a44fc93638d8802763e70251
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-09-01 08:08:59 -0400

    Module 2 Hello World

--- Modules/Module2/HelloWorld.py
+++ Modules/Module2/HelloWorld.py
@@ -5,7 +5,6 @@ current_time = now.strftime("%H:%M:%S")
 
 name = input("Hello there! What is your name? ")
 studentId = input("What is your student id? ")
-
 print(f"Hello {name}! ")
 print(f"Your Student id is: {studentId} ")
 print(f"Currently, the time is: {current_time} ")
