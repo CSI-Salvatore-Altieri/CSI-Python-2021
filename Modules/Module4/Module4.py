76a5ad86808a3b8d8b477bb57d6679ce839dec8c
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-09-08 09:26:08 -0400

    Module4 Part 1

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,9 @@
+meal="Croissainwich"
+print("what did you have for breakfast?")
+print(meal)
+meal="Turkey Tetrazzini"
+print("what was your lunch?")
+print(meal)
+meal="Yes, Beef Bourguignon"
+print("Have you had dinner?")
+print(meal)
