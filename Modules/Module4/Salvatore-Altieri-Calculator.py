78c431d9f29db91667588242130b94f3c05ac6ae
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-10-20 08:27:56 -0400

    t

--- Modules/Module4/Salvatore-Altieri-Calculator.py
+++ Modules/Module4/Salvatore-Altieri-Calculator.py
@@ -1,9 +1,16 @@
-# print("Hello World!")
-# print("What is your name?")
-# myName = input()
-# print("It is good to meet you", myName)
-# print("The length of your name is: ", len(myName))
-# print("What is your age?")
-# myAge = input()
-# print("You will be " + str(int(myAge) + 5) + " in 5 years")
+print("Rectangle Perimeter Calculator")
+print("""
+ ----------
+|          |
+|          |
+|          |
+|          |
+|          |
+ ----------
+""")
+width = int(input("What is the width of the rectangle?: "))
+length = int(input("Wht is the length of the rectangle?: "))
+print("The Perimeter of the rectangle is: ", 2 *(width + length))
 
+def PerimeterOfRectangle(width, height):
+    print(f"Area of the rectangle is {2 * int(width) + int(length)}")
