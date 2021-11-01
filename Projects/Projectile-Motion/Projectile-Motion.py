36d31db8a77901953f1cbe34349b405ed11bc94c
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-11-01 08:55:22 -0400

    t

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,15 +1,22 @@
-gun = "Sword International Mk-18.338 LM Marksman Rifle"
-caliber = ".338"
-ammunition = "Lapua Magnum TAC-X"
-velocity_ms = 880
+# gun = "Sword International Mk-18.338 LM Marksman Rifle"
+# caliber = ".338"
+# ammunition = "Lapua Magnum TAC-X"
+# velocity_ms = 880
 
-Building = "Shanghai World Financial Center"
-BuildingHeight = 1614
+# Building = "Shanghai World Financial Center"
+# BuildingHeight = 1614
 
-gravity_ms = 9.81
+# gravity_ms = 9.81
+
+# import math
+
+# time_s = math.sqrt((2 * BuildingHeight) / gravity_ms)
 
 import math
+def ProjectileFunction(gun:str, caliber:str, ammunition:str, velocity_ms: int, Building:str, BuildingHeight: int, gravity_ms: int):
+    time_s = math.sqrt((2 * BuildingHeight) / gravity_ms)
+    distance_m = (velocity_ms * time_s)
 
-time_s = math.sqrt((2 * BuildingHeight) / gravity_ms)
-
-print(f"The gun selected for the expirement is the {gun}. The caliber of the {gun} is a {caliber} {ammunition}. The velocity of the bullet is {velocity_ms}. An i will fire from the {Building} thats height is {BuildingHeight} with the {gun} at the street to kill the CFO and his bodyguards so Shanghai financial empire will crumble")
+    print(f"The gun selected for the expirement is the {gun}. The caliber of the {gun} is a {caliber} {ammunition}. The velocity of the bullet is {velocity_ms}. An i will fire from the {Building} thats height is {BuildingHeight} with the {gun} at the street to kill the CFO and his bodyguards so Shanghai financial empire will crumble")
+    
+ProjectileFunction("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Shanghai World Financial Center", 1614, 9.81)
