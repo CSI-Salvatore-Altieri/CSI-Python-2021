351f46d41f8974cee89f5f9b2760ce91ed1cdb38
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-11-05 09:14:05 -0400

    t

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,10 +1,12 @@
+import math
+from ExperimentalData import ExperimentalData
 # gun = "Sword International Mk-18.338 LM Marksman Rifle"
 # caliber = ".338"
 # ammunition = "Lapua Magnum TAC-X"
 # velocity_ms = 880
 
-# Building = "Shanghai World Financial Center"
-# BuildingHeight = 1614
+# building = "Shanghai World Financial Center"
+# buildingHeight = 1614
 
 # gravity_ms = 9.81
 
@@ -12,11 +14,27 @@
 
 # time_s = math.sqrt((2 * BuildingHeight) / gravity_ms)
 
-import math
-def ProjectileFunction(gun:str, caliber:str, ammunition:str, velocity_ms: int, Building:str, BuildingHeight: int, gravity_ms: int):
-    time_s = math.sqrt((2 * BuildingHeight) / gravity_ms)
-    distance_m = (velocity_ms * time_s)
+def ProjectileFunction(experimentalData: ExperimentalData):
 
-    print(f"The gun selected for the expirement is the {gun}. The caliber of the {gun} is a {caliber} {ammunition}. The velocity of the bullet is {velocity_ms}. An i will fire from the {Building} thats height is {BuildingHeight} with the {gun} at the street to kill the CFO and his bodyguards so Shanghai financial empire will crumble")
+    time_s = math.sqrt((2 * experimentalData.buildingHeight) / experimentalData.gravity_ms)
+
+    distance_m = (experimentalData.velocity_ms * time_s)
+    # distance_m = (experimentalData[velocity_ms] * time_s)
+
+    print(f"The gun selected for the expirement is the {experimentalData.gun}. The caliber of the {experimentalData.gun} is a {experimentalData.caliber} {experimentalData.ammunition}. The velocity of the bullet is {experimentalData.velocity_ms}. An i will fire from the {experimentalData.building} thats height is {experimentalData.buildingHeight} with the {experimentalData.gun} at the street to kill the CFO and his bodyguards so Shanghai financial empire will crumble")
     
-ProjectileFunction("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Shanghai World Financial Center", 1614, 9.81)
+# experimentalData = {
+
+# "gun": "Sword International Mk-18.338 LM Marksman Rifle",
+# "caliber": ".338",
+# "ammunition": "Lapua Magnum TAC-X",
+# "velocity_ms": 880,
+# "building": "Shanghai World Financial Center",
+# "buildingHeight": 1614,
+# "gravity_ms": 9.81
+
+# }
+
+experimentalData = ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Shanghai World Financial Center", 1614, 9.81)
+
+ProjectileFunction
