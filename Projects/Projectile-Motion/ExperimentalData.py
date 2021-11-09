b424d70f24acf1dc272781d0a67c145ab30b6bd1
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-11-09 08:31:32 -0400

    t

---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self, gun:str, caliber:str, ammunition:str, velocity_ms: int, building:str, buildingHeight: int, gravity_ms):
+        self.gun = gun
+        self.caliber = caliber
+        self.ammunition = ammunition
+        self.velocity_ms = velocity_ms
+        self.building = building
+        self.buildingHeight = buildingHeight
+        self.gravity_ms = gravity_ms
