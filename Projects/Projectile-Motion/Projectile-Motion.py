9ee9bd52974d663e873124483fb797d44eda4435
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2021-11-09 17:50:00 -0400

    t

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,5 +1,9 @@
 import math
+import os
+from pathlib import Path
 from ExperimentalData import ExperimentalData
+import json
+
 # gun = "Sword International Mk-18.338 LM Marksman Rifle"
 # caliber = ".338"
 # ammunition = "Lapua Magnum TAC-X"
@@ -10,19 +14,17 @@ from ExperimentalData import ExperimentalData
 
 # gravity_ms = 9.81
 
-# import math
+#Convert your variables into parameters
 
-# time_s = math.sqrt((2 * BuildingHeight) / gravity_ms)
-
-def ProjectileFunction(experimentalData: ExperimentalData):
-
+def projectileFunction(experimentalData: ExperimentalData):
     time_s = math.sqrt((2 * experimentalData.buildingHeight) / experimentalData.gravity_ms)
 
     distance_m = (experimentalData.velocity_ms * time_s)
     # distance_m = (experimentalData[velocity_ms] * time_s)
 
     print(f"The gun selected for the expirement is the {experimentalData.gun}. The caliber of the {experimentalData.gun} is a {experimentalData.caliber} {experimentalData.ammunition}. The velocity of the bullet is {experimentalData.velocity_ms}. An i will fire from the {experimentalData.building} thats height is {experimentalData.buildingHeight} with the {experimentalData.gun} at the street to kill the CFO and his bodyguards so Shanghai financial empire will crumble")
-    
+
+#Convert your script parameters into a single JSON Object
 # experimentalData = {
 
 # "gun": "Sword International Mk-18.338 LM Marksman Rifle",
@@ -35,6 +37,28 @@ def ProjectileFunction(experimentalData: ExperimentalData):
 
 # }
 
-experimentalData = ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Shanghai World Financial Center", 1614, 9.81)
+# experimentalData = ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Shanghai World Financial Center", 1614, 9.81)
 
-ProjectileFunction
+
+myDataSet = [
+ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Shanghai World Financial Center", 1614, 9.81),
+ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Central Park Tower", 1550, 9.81),
+ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Petronas Twin Towers", 1483, 9.81),
+ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "KK100", 1449, 9.81),
+ExperimentalData("Sword International Mk-18.338 LM Marksman Rifle", ".338", "Lapua Magnum TAC-X", 880, "Empire State Building", 1250, 9.81)
+]
+
+for data in myDataSet:
+    print("\n-----------------------------------------------\n")
+    projectileFunction(data)
+
+#Serialization
+myOutputPath = Path(__file__). parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'Projectile-Motion.json')
+
+print(myOutputFilePath)
+
+with open(myOutputFilePath, 'w') as outfile:
+    json.dump([data.__dict__ for data in myDataSet], outfile)
+
+# projectileFunction(experimentalData)
