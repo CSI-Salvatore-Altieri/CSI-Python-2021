86b72c1a4d2c3ac53f566ca006dbd118d0dae19c
CSI-Salvatore-Altieri <89258931+CSI-Salvatore-Altieri@users.noreply.github.com>
2022-02-03 08:47:35 -0400

    fdrf

--- Projects/Salvatore-Altieri-Hangman.py
+++ Projects/Salvatore-Altieri-Hangman.py
@@ -1,23 +1,24 @@
 import random
-word_list = ["Buenos Aires, Brasília, San Salvador, Cayenne, Sucre, Bogotá, Fort De France, Caracas, Santiago, Basse Terre"]
+word_list = ["Buenos Aires", "Brasília", "San Salvador", "Cayenne", "Sucre", "Bogotá", "Fort De France", "Caracas", "Santiago", "Basse Terre"]
 
-def get_world(world_list):
+def get_word(word_list):
     word = random.choice(word_list)
     return word.upper()
 
+
 def play(word):
-    word_completion = "_" * len(word)
+    word_completion = "-" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
     tries = 6
     print("Let's play Hang Hangman")
-    print("Theme: Capitals of Latin America words")
+    print("Theme: Capitals of Latin America")
     print(display_hangman(tries))
     print(word_completion)
-    [print("\n")]
+    print("\n")
     while not guessed and tries > 0: 
-        guess = input("Guess a letter or word").upper()
+        guess = input("Guess a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("You already tried", guess, "!")
@@ -25,6 +26,35 @@ def play(word):
                 print("This isnt the word your looking for:(")
                 tries -= 1
                 guessed_letters.append(guess)
+            else:
+                print("Nice one", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "-" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already tried", guess, "!")
+            elif guess != word:
+                print(guess, "isn't the word")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("invalid input")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Good job, you guessed the word!!!")
+    else:
+        print("I'm sorry, but you ran out of tries. The word was " + word + ". Try again")
 
 def display_hangman(tries):
     stages = [   """
@@ -36,6 +66,25 @@ def display_hangman(tries):
                        |   / \\
                        -
                        """,
+                       """
+                       --------
+                       |    |
+                       |    O
+                       |   /|\\
+                       |    |
+                       |   / 
+                       -
+                       """,
+                       """
+                       --------
+                       |    |
+                       |    O
+                       |   /|\\
+                       |    |
+                       |   
+                       -
+                       """,
+                       
                        """
                         --------
                        |    |
@@ -72,4 +121,16 @@ def display_hangman(tries):
                        |
                        -
                        """
-]                  
+    ]  
+    return stages[tries]
+def main():
+    word = get_word(word_list)
+    play(word)
+    while input("Again? (Y/N)").upper() == "Y":
+        word = get_word(word_list)
+        play(word)
+        
+if __name__ == "__main__":
+    main()
+        
+        
